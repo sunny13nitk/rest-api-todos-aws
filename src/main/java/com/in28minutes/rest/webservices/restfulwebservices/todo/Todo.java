c864253c8a1c9cbb4f34413ac3fb193ba0d2a1f7
Sunny Bhardwaj <sunny13nitk@gmail.com>
2024-04-04 16:07:41 +0530

    rest api changes

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/Todo.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/Todo.java
@@ -2,6 +2,8 @@ package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,9 +16,11 @@ public class Todo
 
 	private int id;
 
+	@Size(min = 5)
 	private String username;
-
+	@Size(min = 5)
 	private String description;
+	@FutureOrPresent
 	private LocalDate targetDate;
 	private boolean done;
 
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/rest/ToDoAPI.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/rest/ToDoAPI.java
@@ -1,5 +1,9 @@
 package com.in28minutes.rest.webservices.restfulwebservices.todo.rest;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -7,22 +11,27 @@ import java.util.Optional;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.in28minutes.rest.webservices.restfulwebservices.exceptions.ToDoNotFoundException4User;
 import com.in28minutes.rest.webservices.restfulwebservices.todo.Todo;
 import com.in28minutes.rest.webservices.restfulwebservices.todo.TodoService;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
+import jakarta.validation.Valid;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -114,4 +123,89 @@ public class ToDoAPI
 
     }
 
+    @DeleteMapping("/{userName}/todos/{id}")
+    public ResponseEntity<Void> deleteToDo4UserbyToDoId(@PathVariable String userName, @PathVariable int id)
+    {
+
+        if (StringUtils.hasText(userName))
+        {
+
+            // Get Posts for User
+            if (todoSrv != null)
+            {
+                Optional<Todo> currToDoO = todoSrv.findById(id);
+                if (currToDoO.isPresent())
+                {
+                    todoSrv.deleteById(id);
+                    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                }
+                else
+                {
+                    throw new ToDoNotFoundException4User("No ToDo with Id : " + id + " found for User : " + userName);
+                }
+            }
+
+        }
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @PostMapping("/{userName}/todos")
+    public ResponseEntity<EntityModel<Todo>> createToDo4User(@PathVariable String userName,
+            @Valid @RequestBody @NonNull Todo todo)
+    {
+        EntityModel<Todo> eM = null;
+        HttpHeaders headers = null;
+
+        // SAve the Entity
+        Todo newToDo = todoSrv.addTodo(userName, todo.getDescription(), todo.getTargetDate(), todo.isDone());
+        if (newToDo != null)
+        {
+            eM = EntityModel.of(newToDo);
+            WebMvcLinkBuilder link2UserToDos = linkTo(methodOn(this.getClass()).retrieveTodos4User(userName));
+            eM.add(link2UserToDos.withRel(relUserTodos));
+
+            // Create a new HttpHeaders object
+            headers = new HttpHeaders();
+
+            // Set Header location using current Path
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(newToDo.getId()).toUri();
+            headers.setLocation(location);
+        }
+
+        return new ResponseEntity<>(eM, headers, HttpStatus.CREATED);
+
+    }
+
+    @PutMapping("/{userName}/todos/{id}")
+    public ResponseEntity<EntityModel<Todo>> changeToDo4UserbyToDoId(@PathVariable String userName,
+            @PathVariable int id, @Valid @RequestBody @NonNull Todo todo)
+    {
+
+        EntityModel<Todo> eM = null;
+        HttpHeaders headers = null;
+
+        if (todo != null)
+        {
+            todoSrv.updateTodo(todo);
+
+            eM = EntityModel.of(todo);
+
+            WebMvcLinkBuilder link2UserToDos = linkTo(methodOn(this.getClass()).retrieveTodos4User(userName));
+            eM.add(link2UserToDos.withRel(relUserTodos));
+
+            // Create a new HttpHeaders object
+            headers = new HttpHeaders();
+
+            // Set Header location using current Path
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(todo.getId()).toUri();
+            headers.setLocation(location);
+        }
+
+        return new ResponseEntity<>(eM, headers, HttpStatus.OK);
+
+    }
 }
